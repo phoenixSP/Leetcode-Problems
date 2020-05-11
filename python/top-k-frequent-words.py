a800b9d503fc4ed6d031802b2073e26024d610a4
phoenixSP <pal00007@umn.edu>
2020-05-11 12:54:03 -0500

    Update top-k-frequent-words.py

--- python/top-k-frequent-words.py
+++ python/top-k-frequent-words.py
@@ -5,8 +5,18 @@ Created on Mon May 11 11:53:42 2020
 @author: shrey
 """
 
-
+class Element:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+        
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word
+        return self.freq < other.freq
+    
 class Solution:
+    
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         
         
